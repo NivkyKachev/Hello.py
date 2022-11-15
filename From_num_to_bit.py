d2fd29228ab72532602102f588c1bd361fdbe40c
Nikolay Kachev CSE (37 group) <61081551+NivkyKachev@users.noreply.github.com>
2022-11-16 00:02:39 +0200

    Create From_num_to_bit.py

---
+++ From_num_to_bit.py
@@ -0,0 +1,22 @@
+print("Enter a number:")
+num = int(input())
+
+print("Enter bit:")
+bit = int(input())
+
+str = ""
+
+while num > 0:
+    if num % 2 == 1:
+        str += "1"
+    else:
+        str += "0"
+    num = num // 2
+
+print("The number in binary number system is:")
+print(str[::-1])
+
+if str == "1":
+    print("1")
+else:
+    print("0")
