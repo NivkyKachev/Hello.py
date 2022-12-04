3c2674b1fbcc7b482e9e131b05a4a0ce5cdbdea2
Nikolay Kachev CSE 37A <61081551+NivkyKachev@users.noreply.github.com>
2022-12-04 15:52:10 +0200

    Create zad.py

---
+++ Sem_05/zad.py
@@ -0,0 +1,14 @@
+class MissingParameterError(Exception):
+    pass
+
+class InvalidParameterError(Exception):
+    pass
+
+class InvalidAgeError(InvalidParameterError):
+    pass
+
+class InvalidSoundError(InvalidParameterError):
+    pass
+
+class JungleAnimal:
+    def 
